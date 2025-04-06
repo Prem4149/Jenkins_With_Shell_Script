421f9c21fc2cb0ec76f2e3f92fc5ac7a3162958f
Premsing Yadav <131946052+Prem4149@users.noreply.github.com>
2025-04-06 15:40:27 +0530

    Update ShellScript_file1.sh

--- ShellScript_file1.sh
+++ ShellScript_file1.sh
@@ -17,12 +17,4 @@ echo "🔥 Top 5 Processes by CPU:"
 ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%cpu | head -n 6
 
 echo "==============================="
-echo "🛠️  Active Services:"
-systemctl list-units --type=service --state=running | head -n 10
-
-echo "==============================="
-echo "🌐 IP Address:"
-hostname -I
-
-echo "==============================="
 echo "✅ Script Completed."
